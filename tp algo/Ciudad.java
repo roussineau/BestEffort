4e1e5190cf883a4e2bddd1ad1ee75f3c0a6a84a9
Martina <alfonsosuarezmartina@gmail.com>
2024-11-11 16:00:32 -0300

    clase ciudad y traslado

---
+++ tp algo/Ciudad.java
@@ -0,0 +1,27 @@
+
+ 
+ public class Ciudad {
+
+    private int identificador;  
+    private int ganancia ;
+    private int superavit;
+
+   public Ciudad (int identificador){
+     this.identificador = identificador;
+     this.superavit = 0;
+     this.ganancia = 0;
+   }
+
+   public int ganancia (){
+    return ganancia;
+   }
+
+   public int superavit (){
+    return superavit;
+   }
+
+   public int identificador (){
+    return identificador;
+   }
+
+ }
